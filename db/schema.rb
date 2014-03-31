0b9b87dced520f49a273099ce7d69699c2b93b53
Jordan Acuna <jordan.acuna@gmail.com>
2014-03-31 16:01:04 -0500

    Generated an order model and ran the associated migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140331205749) do
+ActiveRecord::Schema.define(version: 20140331210011) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -69,6 +69,16 @@ ActiveRecord::Schema.define(version: 20140331205749) do
     t.integer  "publisher_id"
     t.integer  "genre_id"
     t.integer  "developer_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "orders", force: true do |t|
+    t.string   "status"
+    t.decimal  "pst_rate"
+    t.decimal  "gst_rate"
+    t.decimal  "hst_rate"
+    t.integer  "cust_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

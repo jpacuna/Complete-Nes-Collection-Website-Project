9599302af7bd7b99c6c2b07b20315b58c48b0979
Jordan Acuna <jordan.acuna@gmail.com>
2014-03-31 15:43:46 -0500

    Created Complete-Nes Collection Project File.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CompleteNesCollection::Application.config.secret_key_base = '4675638421fffa64da347e7f69db92aa0ffba52774180d04975466314f2c80111ed88645d800b25d89c6f1481001a3c82cff4607dc83e8efae15394068c454bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CompleteNesCollection::Application.config.session_store :cookie_store, key: '_complete-nes-collection_session'

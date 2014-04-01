c7c97c094e4179062ccd97abbf43b7313a926ce6
Jordan Acuna <jordan.acuna@gmail.com>
2014-04-01 15:40:08 -0500

    Added associations to game.rb.

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,7 @@ class Game < ActiveRecord::Base
   validates :name, :reg_price, :sale_price, :stock_quantity, :release_year, presence: true
   validates :reg_price, :sale_price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :stock_quantity, numericality: { only_integer: true }
+  belongs_to :genre
+  belongs_to :developer
+  belongs_to :publisher
 end

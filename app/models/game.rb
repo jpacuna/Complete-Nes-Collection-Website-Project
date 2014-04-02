e3eb4e63880832bfc58b25d8bca401ab13d137c3
Jordan Acuna <jordan.acuna@gmail.com>
2014-04-02 10:15:30 -0500

    Modified views and layout to dry up code.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ActiveRecord::Base
-  validates :name, :reg_price, :sale_price, :stock_quantity, :release_year, presence: true
+  validates :name, :reg_price, :sale_price, :stock_quantity, :release_year, :developer_id, :genre_id, :publisher_id, presence: true
   validates :reg_price, :sale_price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :stock_quantity, numericality: { only_integer: true }
   belongs_to :genre
